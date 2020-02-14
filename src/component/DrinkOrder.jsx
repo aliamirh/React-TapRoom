03b48e1bb875b9f95875586f06013f6d1cd5b1e5
Ali Amirhamzeh <aliamirhamzeh@gmail.com>
2020-02-14 13:19:06 -0800

    debugging

--- src/component/DrinkOrder.jsx
+++ src/component/DrinkOrder.jsx
@@ -5,12 +5,23 @@ class DrinkOrder extends React.Component {
     state = {
         order: 0
     };
+
+    incrementOrder = () => {
+        this.setState( prevState =>  ({           
+                order: prevState.order + 1         
+        }));
+    }
+    decrementOrder = () => {
+        this.setState( prevState => ({
+                order: prevState.order -1,     
+        }));
+    }
     render(){
         return (
             <div>
-                <button>-</button>
+                <button onClick={this.decrementOrder}>-</button>
                 <span>{this.state.order}</span>
-                <button>+</button>
+                <button onClick={this.incrementOrder}>+</button>
                 <div><Link to='/'>Home</Link></div>
             </div>
         );
